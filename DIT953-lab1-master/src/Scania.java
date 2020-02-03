706e0535a8f0c33146ac1289dc6f7373b6a9e6e7
Yu-ShinH <yushin_hua@hotmail.com>
2020-02-03 14:58:58 +0100

    ..

---
+++ DIT953-lab1-master/src/Scania.java
@@ -0,0 +1,26 @@
+import java.awt.*;
+
+/**
+ * Represents a Scania
+ * @author Yu Shin Hua
+ * @author Marja Rolandsson
+ * @author Simon Genne
+ */
+public class Scania extends car{
+    /**
+     * Constructor of Scania objects.
+     */
+    public Scania() {
+        super(2,Color.red,100, "Scania");
+    }
+
+    @Override
+    public void gas(double amount) {
+
+    }
+
+    @Override
+    public void brake(double amount) {
+
+    }
+}
