c8fbd9b157a3133473f430a26462508cd0d33eb0
mrrj <marja.rolandsson@gmail.com>
2020-02-10 15:18:51 +0100

    Add files via upload

---
+++ Transporter.java
@@ -0,0 +1,69 @@
+import java.util.List;
+
+/**
+ * Dependency class used by classes that are transporters. An interface extending Movable has to be created for every
+ * class depending on Transporter.
+ * @param <T> the interface implemented by classes that can be transported by the dependent class (has to extend Movable).
+ * @author Yu Shin Hua
+ * @author Marja Rolandsson
+ * @author Simon Genne
+ */
+public class Transporter<T extends Movable> {
+    private List<T> storage;
+    private int max;
+
+    private Mover mover;
+
+    /**
+     * Constructor for Transporter objects.
+     * @param storage a List used for storing the given objects.
+     * @param max the max amount of objects that can be stored.
+     * @param mover the same Mover object used by dependent class.
+     */
+    public Transporter(List<T> storage, int max, Mover mover) {
+        this.storage = storage;
+        this.max = max;
+        this.mover = mover;
+    }
+
+    /**
+     * Stores an object if the maximum capacity has not been reached.
+     * @param t the object to be stored.
+     */
+    public void load(T t) {
+        if (storage.size() < max) {
+            storage.add(t);
+        }
+        else {
+            System.out.println("Maximum storage capacity has been reached.");
+        }
+    }
+
+    /**
+     * @return one object storage.
+     */
+    public T unload() {
+            return storage.remove(0);
+    }
+
+    /**
+     * @return the number of objects currently being stored.
+     */
+    public int getN() {
+        return storage.size();
+    }
+
+    public T getStorage(int i){ return storage.get(i);}
+
+    /**
+     * Changes the xy-coordinates of the dependent object and all stored objects.
+     */
+    public void move() {
+        mover.move();
+
+        for (Movable m : storage) {
+            m.setX(mover.getX());
+            m.setY(mover.getY());
+        }
+    }
+}
